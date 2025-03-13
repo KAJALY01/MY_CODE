72928ce9bc8ad0903d60b7aedf7506dec0bbe445
Kajal Yadav <kajaly@amdocs.com>
2025-03-13 10:06:10 +0530

    2nd program

---
+++ A55.java
@@ -0,0 +1,14 @@
+package Access_Apecifier;
+
+import Access_specifier2.Student;
+
+public class A55 {
+
+	public static void main(String[] args) {
+		Student.add();//only public can access
+		Student.sub();
+		Student.mul();
+		Student.div();
+		
+	}
+}
